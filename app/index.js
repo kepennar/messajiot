28c3b532ea14e654f8208b0584d8ea239f9ceb1f
kepennar <kevin.pennarun@gmail.com>
2015-07-30 11:09:09 +0200

    add cors

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ var rqr = require('rqr');
 
 var express = require('express');
 var loggerStream = rqr('app/components/logger').stream;
+var cors = require('cors');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var compress = require('compression');
@@ -15,6 +16,7 @@ var env = process.env.NODE_ENV || 'dev';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'dev';
 
+app.use(cors);
 app.use(morgan('dev', {
   'stream': loggerStream
 }));
